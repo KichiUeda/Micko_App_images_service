db0139977963ad8bd0a4da004ec6c2fcb6c03cbd
nightlaro <mickocabacungan@gmail.com>
2020-06-20 00:05:34 -0700

    Database set up

---
+++ database/model.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/images',  {useNewUrlParser: true, useUnifiedTopology: true})
+
+const db = mongoose.connection;
+db.on('error', () => {
+  console.log('connection error');
+})
+db.once('open', () => {
+  console.log('connected to mongodb')
+})
+
+const images = new mongoose.Schema({
+  carousel_images: [String],
+  carousel_videos: [String],
+  description_images: [String],
+  description_gifs: [String],
+  thumbnail: String,
+  cover_image: String
+})
+
+const Images = mongoose.model('Images', images);
+
+module.exports = Images;
