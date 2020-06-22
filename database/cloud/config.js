02f63883fb61e34cbfc854caa1dcabbb200d5521
nightlaro <mickocabacungan@gmail.com>
2020-06-21 22:46:58 -0700

    added uploading script, uploaded images and videos to cloudinary service

---
+++ database/cloud/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  cloudinary: {
+    cloud_name: 'fec-image-services',
+    api_key: '611813362779935',
+    api_secret: 'T7ZOuCHU_cJHKTlS_Z1Z9zMxcaY'
+  },
+  pexels: {
+    key: '563492ad6f917000010000016ef54e669ed54a628cf9b9047ab7e6b1'
+  }
+};
