45fecdf60fd042a9178c3f855258f3f88e45d776
nightlaro <mickocabacungan@gmail.com>
2020-06-24 21:22:18 -0700

    fixed based on suggestion from code review

--- client/index.jsx
+++ client/index.jsx
@@ -1,21 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
-class App extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      data: []
-    }
-
-  }
-
-
-  render() {
-    return <div>
-        <h1>HELLO WORLD</h1>
-      </div>
-  }
-}
+import App from "./components/App"
 
 ReactDOM.render(<App />, document.getElementById('app'));
