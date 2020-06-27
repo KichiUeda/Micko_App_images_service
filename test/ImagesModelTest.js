fe95e32ecef6c4f577dfc8dbd476f8b435d995b3
nightlaro <mickocabacungan@gmail.com>
2020-06-26 18:33:58 -0700

    attempt to implement travis ci

--- test/ImagesModelTest.js
+++ test/ImagesModelTest.js
@@ -6,9 +6,23 @@ const sampleData = require('../database/sampleDatas/imageServiceSample')
 describe('Images model should save a document', () => {
 
   var data = sampleData[0];
+  var {product_id, carousel_images, carousel_videos, description_images, description_gifs, thumbnail, cover_image} = sampleData[0];
+  var newDocument = new Images ({
+    product_id, carousel_images, carousel_videos, description_images, description_gifs, thumbnail, cover_image
+  })
 
   it('save a document in mongoDB using image model', () => {
-    expect(data).to.deep.equal(data)
+    Images.deleteMany({}, () => {
+      console.log('Deleted all documents from Images collection');
+
+      newDocument.save((err, document) => {
+        var {product_id, carousel_images, carousel_videos, description_images, description_gifs, thumbnail, cover_image} = document;
+
+        var documentObj = {product_id, carousel_images, carousel_videos, description_images, description_gifs, thumbnail, cover_image}
+        console.log(documentObj);
+        expect(data).to.deep.equal(documentObj)
+      })
+    })
   })
 
 })
