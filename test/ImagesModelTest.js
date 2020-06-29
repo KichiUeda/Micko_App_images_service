1445576a2acf943916231407ca33ae0342a6e2de
nightlaro <mickocabacungan@gmail.com>
2020-06-28 17:53:52 -0700

    modified ImagesModelTest

--- test/ImagesModelTest.js
+++ test/ImagesModelTest.js
@@ -1,28 +1,14 @@
 const Images = require('../database/imagesModel');
+const mongoose = require('mongoose');
 const assert = require('chai').assert;
 const expect = require('chai').expect;
-const sampleData = require('../database/sampleDatas/imageServiceSample')
 
-describe('Images model should save a document', () => {
-
-  var data = sampleData[0];
-  var {product_id, carousel_images, carousel_videos, description_images, description_gifs, thumbnail, cover_image} = sampleData[0];
-  var newDocument = new Images ({
-    product_id, carousel_images, carousel_videos, description_images, description_gifs, thumbnail, cover_image
-  })
-
-  it('save a document in mongoDB using image model', () => {
-    Images.deleteMany({}, () => {
-      console.log('Deleted all documents from Images collection');
-
-      newDocument.save((err, document) => {
-        var {product_id, carousel_images, carousel_videos, description_images, description_gifs, thumbnail, cover_image} = document;
-
-        var documentObj = {product_id, carousel_images, carousel_videos, description_images, description_gifs, thumbnail, cover_image}
-        console.log(documentObj);
-        expect(data).to.deep.equal(documentObj)
-      })
-    })
-  })
-
-})
+describe('Mongoose model set up correctly', () => {
+  it('Images should have an instance of Mongoose Schema', () => {
+    expect(Images.images).to.be.instanceof(mongoose.Schema);
+  });
+  console.log(mongoose.model);
+  it('Images should have a model', () => {
+    expect(Images.Images).to.be.instanceof(Function);
+  });
+});
