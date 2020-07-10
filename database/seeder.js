6ec9261c6bb6c557c44b1ee7bef50e587704cd85
nightlaro <mickocabacungan@gmail.com>
2020-07-09 17:39:39 -0700

    re-wrote seeder to only call API 4x per seed run"

--- database/seeder.js
+++ database/seeder.js
@@ -8,8 +8,14 @@ cloudinary.config({
   api_secret: process.env.CLOUD_SECRET
 });
 
-var randomVideoPackage = (quantity) => {
-  var videoPackage = [];
+//MEDIA ARRAY CONTAINERS
+var videos = [];
+var coverImages = [];
+var descriptionImages = [];
+var carouselImages = [];
+
+var randomVideoPackage = () => {
+  //var videoPackage = [];
   var max_results = 30
   return new Promise ((resolve,reject) => {
     cloudinary.search
@@ -17,14 +23,14 @@ var randomVideoPackage = (quantity) => {
     .max_results(max_results)
     .execute().then(results => {
       var videosArray = results.resources;
-      for (var i = 0; i < quantity; i++) {
-        var randomPicker = Math.floor(Math.random() * max_results);
+      for (var i = 0; i < max_results; i++) {
+        //var randomPicker = Math.floor(Math.random() * max_results);
         //console.log(videosArray[randomPicker].url);
-        videoPackage.push(videosArray[randomPicker].url);
+        videos.push(videosArray[i].url);
       }
       //console.log(videoPackage);
 
-      resolve(videoPackage);
+      resolve(videos);
     })
     .catch(err => {
       reject(err);
@@ -32,8 +38,8 @@ var randomVideoPackage = (quantity) => {
   })
 }
 
-var randomCoverImages = (quantity) => {
-  var coverImages = [];
+var randomCoverImages = () => {
+  //var coverImages = [];
   var max_results = 60
 
   return new Promise ((resolve,reject) => {
@@ -42,13 +48,13 @@ var randomCoverImages = (quantity) => {
     .max_results(max_results)
     .execute().then(results => {
       var resultsArray = results.resources;
-      for (var i = 0; i < quantity; i++) {
-        var randomPicker = Math.floor(Math.random() * max_results);
+      for (var i = 0; i < max_results; i++) {
+        //var randomPicker = Math.floor(Math.random() * max_results);
         //console.log(videosArray[randomPicker].url);
-        coverImages.push(resultsArray[randomPicker].url);
+        coverImages.push(resultsArray[i].url);
       }
       //console.log(coverImages);
-      resolve(coverImages[0])
+      resolve(coverImages)
     })
     .catch(err => {
       console.log(err);
@@ -57,8 +63,8 @@ var randomCoverImages = (quantity) => {
   })
 }
 
-var randomDescriptionImages = (quantity) => {
-  var descriptionImages = [];
+var randomDescriptionImages = () => {
+  //var descriptionImages = [];
   var max_results = 60
 
   return new Promise((resolve,reject) => {
@@ -67,10 +73,10 @@ var randomDescriptionImages = (quantity) => {
     .max_results(max_results)
     .execute().then(results => {
       var resultsArray = results.resources;
-      for (var i = 0; i < quantity; i++) {
-        var randomPicker = Math.floor(Math.random() * max_results);
+      for (var i = 0; i < max_results; i++) {
+        //var randomPicker = Math.floor(Math.random() * max_results);
         //console.log(videosArray[randomPicker].url);
-        descriptionImages.push(resultsArray[randomPicker].url);
+        descriptionImages.push(resultsArray[i].url);
       }
       //console.log(descriptionImages);
       resolve(descriptionImages);
@@ -82,8 +88,8 @@ var randomDescriptionImages = (quantity) => {
   })
 }
 
-var randomCarouselImages = (quantity) => {
-  var carouselImages = [];
+var randomCarouselImages = () => {
+  //var carouselImages = [];
   var max_results = 60
 
   return new Promise((resolve,reject) => {
@@ -92,10 +98,10 @@ var randomCarouselImages = (quantity) => {
     .max_results(max_results)
     .execute().then(results => {
       var resultsArray = results.resources;
-      for (var i = 0; i < quantity; i++) {
-        var randomPicker = Math.floor(Math.random() * max_results);
+      for (var i = 0; i < max_results; i++) {
+        //var randomPicker = Math.floor(Math.random() * max_results);
         //console.log(videosArray[randomPicker].url);
-        carouselImages.push(resultsArray[randomPicker].url);
+        carouselImages.push(resultsArray[i].url);
       }
       //console.log(carouselImages);
       resolve(carouselImages);
@@ -106,31 +112,45 @@ var randomCarouselImages = (quantity) => {
     })
   })
 }
-
-for (var i = 0; i < 5; i++) {
-  //created 4 random number variables to generate random quantity for each 'randomFunc'
-  var randomCarousel = Math.floor(Math.random() * 10);
-  var randomVideo = Math.floor(Math.random() * 4);
-  var randomDescription = Math.floor(Math.random() * 10);
-  var id = 0;
-
-  Promise.all([randomCarouselImages(randomCarousel),randomVideoPackage(randomVideo), randomDescriptionImages(randomDescription), randomCoverImages(1)])
-  .then((packages) => {
-    console.log(packages);
-    var imageDocument = new Images ({
-      product_id: id,
-      carousel_images: packages[0],
-      carousel_videos: packages[1],
-      description_images: packages[2],
-      description_gifs: packages[1], //still need to figure out using a gif provider to upload gifs to cloud
-      thumbnail: packages[3], //cloud provides a way to resize cover_images, will implement this later
-      cover_image: packages[3]
-    })
-    id++;
-    imageDocument.save((err, document) => {
-      if (err) {console.log(err)}
-      console.log("Saved document")
-    })
-  })
-  .catch(err => console.log(err))
+//random picker helper function
+var randomPicker = (mediaArray, amount) => {
+  var result = [];
+  for (var i = 0; i < amount; i++) {
+    var randomGenerator = Math.floor(Math.random() * mediaArray.length);
+    result.push(mediaArray[randomGenerator]);
+  }
+  return result;
 }
+
+//amount of seeding = the amount of documents to save to mongodb
+var packageForDatabase = (amountOfSeeding) => {
+  var randomCarouselAmount = Math.floor(Math.random() * 6);
+  var randomVideoAmount = Math.floor(Math.random() * 4);
+  var randomDescriptionAmount = Math.floor(Math.random() * 4);
+  var id = 1;
+  Promise.all([randomCarouselImages(randomCarousel),randomVideoPackage(randomVideo), randomDescriptionImages(randomDescription), randomCoverImages(1)])
+  .then(() => {
+    for (var i = 0; i < amountOfSeeding; i++) {
+      var imageDocument = new Images ({
+        product_id: id,
+        carousel_images: randomPicker(carouselImages, randomCarouselAmount),
+        carousel_videos: randomPicker(videos, randomVideoAmount),
+        description_images: randomPicker(descriptionImages, randomDescriptionAmount),
+        description_gifs: descriptionImages[0], //still need to figure out using a gif provider to upload gifs to cloud
+        thumbnail: randomPicker(coverImages, 1),
+        cover_image: randomPicker(coverImages, 1),
+      })
+
+      id++;
+
+      imageDocument.save((err, document) => {
+        if (err) {console.log(err)}
+        console.log("Saved document")
+      });
+    }
+  })
+  .catch(err => console.log(err));
+}
+
+//Invoke packageForDatabase to start saving to mongoDB
+packageForDatabase(100);
