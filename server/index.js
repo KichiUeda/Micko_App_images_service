0b69c566eb2277bd6c916a5b8c15beb750330698
nightlaro <mickocabacungan@gmail.com>
2020-07-14 22:11:43 -0700

    refactored static serve middleware

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const app = express();
 
 console.log(__dirname);
 app.use(cors());
-app.use(express.static(__dirname + '/public'));
+app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded())
 
