ce64ffc8316178f815237b9124df7e1effca1411
nightlaro <mickocabacungan@gmail.com>
2020-07-31 15:38:52 -0700

    selection css

--- client/components/Cards.jsx
+++ client/components/Cards.jsx
@@ -38,6 +38,9 @@ const Card = styled.div`
   margin-right: 5px;
   margin-top: 15px;
   cursor: pointer;
+  .current {
+    border: 6px solid #4CA0E5;
+  }
   img {
     width: 218px;
     height: 122px;
@@ -57,7 +60,7 @@ export default class Cards extends React.Component {
       if (url.includes('.mp4')) {
         return (
           <Card>
-            <video onClick={this.props.onClick} poster='https://d33v4339jhl8k0.cloudfront.net/docs/assets/591c8a010428634b4a33375c/images/5ab4866b2c7d3a56d8873f4c/file-MrylO8jADD.png'>
+            <video className='current' onClick={this.props.onClick} poster='https://d33v4339jhl8k0.cloudfront.net/docs/assets/591c8a010428634b4a33375c/images/5ab4866b2c7d3a56d8873f4c/file-MrylO8jADD.png'>
               <source src={url} type="video/mp4"></source>
               Your browser does not support the video tag.
             </video>
@@ -66,7 +69,7 @@ export default class Cards extends React.Component {
       } else if (url.includes('.jpg')) {
         return (
           <Card>
-            <img src={url} onClick={this.props.onClick}></img>
+            <img className='current' src={url} onClick={this.props.onClick}></img>
           </Card>
         );
       }
